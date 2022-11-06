602cd351e077970dfdbb6486da6f066ad57925a3
Rangga Rifqi <rangga@ranggarifqi.com>
2022-11-06 12:31:43 +0700

    Updated PrismaUserRepository -> create()

--- src/common/repos/user/prisma.repository.test.ts
+++ src/common/repos/user/prisma.repository.test.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Credential } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 import { truncateTables } from 'src/common/testUtil/teardown';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -10,6 +11,7 @@ import {
 } from 'src/common/testUtil/factories';
 import { Dict } from 'src/common/types';
 import { PrismaUserRepository } from './prisma.repository';
+import { EGender } from './userRepo.interface';
 
 describe('PrismaUserRepository', () => {
   let repository: PrismaUserRepository;
@@ -47,12 +49,20 @@ describe('PrismaUserRepository', () => {
       const dummyUser = await UserFactory.getDummyData({
         id: seeds.credential.id,
         credentialId: seeds.credential.id,
+        gender: EGender.AKHWAT,
+        provinceId: null,
+        districtId: null,
       });
-      const result = await repository.create({ ...dummyUser });
+      const result = await repository.create({
+        ...dummyUser,
+      });
 
       expect(result.id).toBe(seeds.credential.id);
+      expect(result.provinceId).toBeNull();
+      expect(result.districtId).toBeNull();
       expect(result.firstName).toBe(dummyUser.firstName);
       expect(result.lastName).toBe(dummyUser.lastName);
+      expect(result.gender).toBe(dummyUser.gender);
       expect(result.credentialId).toBe(seeds.credential.id);
       expect(result.createdAt).not.toBeNull();
       expect(result.updatedAt).not.toBeNull();
@@ -61,6 +71,47 @@ describe('PrismaUserRepository', () => {
       expect(after).toHaveLength(1);
       expect(after[0].id).toStrictEqual(result.id);
       expect(after[0].firstName).toStrictEqual(result.firstName);
+    });
+
+    it('should create user with related foreign key', async () => {
+      const before = await prismaService.user.findMany();
+      expect(before).toHaveLength(0);
+
+      const dummyUser = await UserFactory.getDummyData({
+        id: seeds.credential.id,
+        credentialId: seeds.credential.id,
+        gender: EGender.AKHWAT,
+        provinceId: '11',
+        districtId: '1101',
+      });
+      const result = await repository.create({
+        ...dummyUser,
+      });
+
+      expect(result.provinceId).toBe('11');
+      expect(result.districtId).toBe('1101');
+
+      const after = await prismaService.user.findMany();
+      expect(after).toHaveLength(1);
+    });
+
+    it('should throw error if related foreign key are not exist', async () => {
+      const dummyUser = await UserFactory.getDummyData({
+        id: seeds.credential.id,
+        credentialId: seeds.credential.id,
+        gender: EGender.AKHWAT,
+        provinceId: 'asd',
+        districtId: 'zzz',
+      });
+
+      try {
+        await repository.create({
+          ...dummyUser,
+        });
+      } catch (error) {
+        expect(error).toBeDefined();
+        expect(error).toBeInstanceOf(PrismaClientKnownRequestError);
+      }
     });
   });
 });
