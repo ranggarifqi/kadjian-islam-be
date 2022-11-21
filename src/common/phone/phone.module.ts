e086639065b8acd97008e63b95b36e305ba1dcc8
Rangga Rifqi <rangga@ranggarifqi.com>
2022-11-21 22:11:03 +0700

    Populate PhoneModule

--- src/common/phone/phone.module.ts
+++ src/common/phone/phone.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { LibPhoneNumberService } from './libPhoneNumber.service';
+import { BasePhoneService } from './phone.interface';
 
-@Module({})
+@Module({
+  providers: [{ provide: BasePhoneService, useClass: LibPhoneNumberService }],
+})
 export class PhoneModule {}
