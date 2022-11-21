6bb3f200994b3c96f061f4aec5cc244bf097c9e9
Rangga Rifqi <rangga@ranggarifqi.com>
2022-11-21 23:09:29 +0700

    Added OrganisationController & AuthModule to OrganisationModule

--- src/organisation/organisation.module.ts
+++ src/organisation/organisation.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { OrganisationController } from './organisation.controller';
 import { OrgCreationModule } from './orgCreation/orgCreation.module';
 
 @Module({
-  imports: [OrgCreationModule],
+  imports: [AuthModule, OrgCreationModule],
+  controllers: [OrganisationController],
 })
 export class OrganisationModule {}
