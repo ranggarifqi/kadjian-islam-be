fb80b67a67b359fb065684e4a8b4035e717dc42a
Rangga Rifqi <rangga@ranggarifqi.com>
2022-11-21 22:29:44 +0700

    Refactored OrganisationController.registerOrganisation

--- src/organisation/organisation.controller.ts
+++ src/organisation/organisation.controller.ts
@@ -9,7 +9,6 @@ import {
 import { Request } from 'express';
 import { JwtAuthGuard } from 'src/auth/strategies/jwt.guard';
 import { IUserCredential } from 'src/auth/strategies/jwt.strategy';
-import { BasePhoneService } from 'src/common/phone/phone.interface';
 import { IOrgRequest } from 'src/common/repos/orgRequest';
 import { SuccessResponse } from 'src/common/response';
 import { RegisterOrgDto } from './organisation.dto';
@@ -17,10 +16,7 @@ import { BaseOrgCreationService } from './orgCreation/orgCreation.interface';
 
 @Controller('organisations')
 export class OrganisationController {
-  constructor(
-    private orgCreationService: BaseOrgCreationService,
-    private phoneService: BasePhoneService,
-  ) {}
+  constructor(private orgCreationService: BaseOrgCreationService) {}
 
   @Post('register')
   @UseGuards(JwtAuthGuard)
@@ -30,32 +26,8 @@ export class OrganisationController {
   ): Promise<SuccessResponse<IOrgRequest>> {
     const credential = request.user as IUserCredential;
 
-    const {
-      name,
-      description,
-      address,
-      mobileNumber,
-      email,
-      size,
-      provinceId,
-      districtId,
-    } = body;
-
-    const { countryCode, phoneNumber } =
-      this.phoneService.splitCountryCodeAndNumber(mobileNumber);
-
     const submittedOrg = await this.orgCreationService.createOrgRequest(
-      {
-        name,
-        description,
-        address,
-        countryCode: countryCode,
-        mobileNumber: phoneNumber,
-        email,
-        size,
-        provinceId,
-        districtId,
-      },
+      body,
       credential.userId,
     );
 
